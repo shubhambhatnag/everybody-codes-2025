8ee0e6b9e7d25dd9656853fb28bc9a3c5b7cd21d
Shubham Bhatnagar <shubham.bhatnagar@pm.me>
2025-12-13 00:43:55 -0500

    fix

--- quest05/part1/main.py
+++ quest05/part1/main.py
@@ -1,41 +1,27 @@
 file = open("input.txt", "r")
 
-smallest = None
-largest = None
-for line in file.readlines():
-    nums = [int(i) for i in line.strip().split(":")[1].split(",")]
+nums = [int(i) for i in file.readlines()[0].strip().split(":")[1].split(",")]
 
-    fishbone = []
+fishbone = []
 
-    for num in nums:
-        placed = False
-        for i in range(len(fishbone)):
-            left, spine, right = fishbone[i]
+for num in nums:
+    placed = False
+    for i in range(len(fishbone)):
+        left, spine, right = fishbone[i]
 
-            if num < spine and left is None:
-                fishbone[i][0] = num
-                placed = True
-                break
-            elif num > spine and right is None:
-                fishbone[i][2] = num
-                placed = True
-                break
-        if not placed:
-            fishbone.append([None, num, None])
+        if num < spine and left is None:
+            fishbone[i][0] = num
+            placed = True
+            break
+        elif num > spine and right is None:
+            fishbone[i][2] = num
+            placed = True
+            break
+    if not placed:
+        fishbone.append([None, num, None])
 
-    num = ""
-    for spine in fishbone:
-        num += str(spine[1])
+num = ""
+for line in fishbone:
+    num += str(line[1])
 
-    num = int(num)
-
-    if largest is None:
-        largest = num
-    else:
-        largest = max(largest, num)
-
-    if smallest is None:
-        smallest = num
-    else:
-        smallest = min(smallest, num)
-print(largest - smallest)
+print(num)
